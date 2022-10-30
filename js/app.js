03b7d271c901c273f4a733d469ec29c5edd0cda7
dstsobrino <dorgis.sobrino@gmail.com>
2022-10-30 20:25:13 -0300

    Actualizo proyecto, casi terminado, solo falta agregar pronostico diarios para los proxinos 5 dias

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ window.addEventListener(`load`, () => {
     let temperaturaValor = document.getElementById("temperatura-valor");
     let sensacionTerm = document.getElementById("sensacion-termica");
     let precipitaciones = document.getElementById("lluvia");
-    let temperaturaDescription = document.getElementById("desc")
+    let temperaturaDescription = document.getElementById("desc");
+    let temperaturaMin = document.getElementById("min");
+    let temperaturaMax = document.getElementById("max");
+    let humedad = document.getElementById("humedad");
 
     let ubicacion = document.getElementById("ubicacion");
     let iconAnimado = document.getElementById("icon-animado");
@@ -15,14 +18,10 @@ window.addEventListener(`load`, () => {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((posicion) => {
-            // console.log(posicion.coords.latitude)
-
             lon = posicion.coords.longitude;
             lat = posicion.coords.latitude;
 
             const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=sp&units=metric&appid=9d30f97fdf83aa6eb5ad7447ba04b150`;
-
-            console.log(url);
 
             fetch(url)
                 .then((response) => {
@@ -30,19 +29,51 @@ window.addEventListener(`load`, () => {
                 })
                 .then((data) => {
                     let temp = Math.round(data.main.temp);
-                    temperaturaValor.textContent = `${temp} º`;
+                    temperaturaValor.textContent = `${temp}º`;
 
-                    console.log(data.weather[0].description)
+                    let desc = data.weather[0].description;
+                    temperaturaDescription.textContent = `${desc}`;
 
-                    let desc = data.weather[0].description
-                    temperaturaDescription.textContent = `${desc}`
-                  
-                    console.log(data.name)
-                    ubicacion.textContent = (data.name)
-                    
+                    ubicacion.textContent = data.name;
 
-                    
-                    
+                    let senc = Math.round(data.main.feels_like);
+                    sensacionTerm.textContent = `${senc}º`;
+
+                    let tmax = Math.round(data.main.temp_max);
+                    temperaturaMax.textContent = `max ${tmax}º/`;
+
+                    let tmin = Math.round(data.main.temp_min);
+                    temperaturaMin.textContent = `min ${tmin}º`;
+
+                    let speed = Math.round(data.wind.speed);
+                    vientoVelocidad.textContent = `${speed} Km/h`;
+
+                    /* let hum = (data.main.humidity)
+                              humedad.textContent = `${hum}%` */
+
+                    switch (data.weather[0].main) {
+                        case "Clouds":
+                            iconAnimado.src = "/multimedia/animated/cloudy.svg";
+                            break;
+                        case "Clear":
+                            iconAnimado.src = "/multimedia/animated/day.svg";
+                            break;
+                        case "Thunderstorm":
+                            iconAnimado.src = "/multimedia/animated/thunder.svg";
+                            break;
+                        /* case "Drizzle":
+                                        iconAnimado.src = "/multimedia/animated/thunder.svg";
+                                    break; */
+                        case "Rain":
+                            iconAnimado.src = "/multimedia/animated/rainy-5.svg";
+                            break;
+                        case "Snow":
+                            iconAnimado.src = "/multimedia/animated/snowy-5.svg";
+                            break;
+
+                        default:
+                            break;
+                    }
                 })
                 .catch((error) => {
                     console.log(error);
