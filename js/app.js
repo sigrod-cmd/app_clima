0eafcc6c4e07cbc013586b2a01eef6853108b4b7
dstsobrino <dorgis.sobrino@gmail.com>
2022-10-31 18:33:58 -0300

    Se modifica proyecto a nivel de organizacion de las carpetas y nombre de los archivos

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ window.addEventListener(`load`, () => {
             lon = posicion.coords.longitude;
             lat = posicion.coords.latitude;
 
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=sp&units=metric&appid=9d30f97fdf83aa6eb5ad7447ba04b150`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=ushuaia&lang=sp&units=metric&appid=9d30f97fdf83aa6eb5ad7447ba04b150`;
 
             fetch(url)
                 .then((response) => {
@@ -54,10 +54,10 @@ window.addEventListener(`load`, () => {
                     switch (data.weather[0].main) {
                         case "Clouds":
                             iconAnimado.src = "/multimedia/animated/cloudy.svg";
-                            break;
+                        break;
                         case "Clear":
                             iconAnimado.src = "/multimedia/animated/day.svg";
-                            break;
+                         break;
                         case "Thunderstorm":
                             iconAnimado.src = "/multimedia/animated/thunder.svg";
                             break;
